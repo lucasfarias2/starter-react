93406eee9df3b8fccdada3be99ae159edc55e0a5
Lucas Farias <lukf95@gmail.com>
2024-01-06 13:09:05 +0100

    Update starter

--- src/shared/pages/App.tsx
+++ src/shared/pages/App.tsx
@@ -22,7 +22,16 @@ function App() {
 
   return (
     <div className="p-8 text-center">
-      <div className="panel mb-36 rounded-2xl bg-white py-16 shadow-lg">
+      <div className="rounded-2xl bg-white py-16 shadow-lg">
+        <div className="my-4 flex items-center justify-center">
+          <img src="https://bun.sh/logo.svg" alt="Bun Logo" className="h-10" />
+          <span className="mx-4 text-2xl text-zinc-400">+</span>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png"
+            alt="React Logo"
+            className="h-10"
+          />
+        </div>
         <h1 className="mb-2 text-5xl font-medium">Packlify Starter-React</h1>
         <p className="text-xl text-zinc-500">
           Bun, Express, React, Vite, Tailwind, Prettier, ESLint, Firebase and React Query
